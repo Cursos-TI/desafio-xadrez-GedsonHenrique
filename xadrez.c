aeb9025ef2c5692f1d30f1a378233d53da134efd
GedsonHenrique <rikinho_coral@hotmail.com>
2025-06-03 00:22:00 +0000

    implementei os movimentos do cavalo

--- xadrez.c
+++ xadrez.c
@@ -8,18 +8,55 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
+    int torre = 0;
+    int bispo = 0;
+    int rainha = 0;
+    int cavalo = 1;
+
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+
+    while (bispo < 5)
+    {
+        printf("Cima Direita\n");
+        bispo++;
+    }
+    printf("\n");
+   
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
+    for (torre; torre < 5; torre++)
+    {
+        printf("Direita\n");
+    }
+    printf("\n");
+
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+
+    do
+    {
+    printf("Esquerda\n"); 
+    rainha++;
+    } while (rainha < 8);
+    printf("\n");
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+
+    while (cavalo--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("baixo\n");
+        }
+        printf("esquerda\n");
+        printf("\n");
+    }
+    
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
